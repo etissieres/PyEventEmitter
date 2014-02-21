5b904ad4dd71624ce1e1c6dc74928a1fec16d24b
etissieres <etienne.tissieres@gmail.com>
2014-02-21 21:42:56 +0100

    improve setup.py & add README.rst to MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,12 +5,22 @@ from distutils.core import setup
 with open('README.rst') as file:
     long_description = file.read()
 
+classifiers = [
+    "Programming Language :: Python",
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved",
+    "Natural Language :: French",
+    "Operating System :: OS Independent",
+    "Topic :: Utilities",
+]
+
 setup(name='PyEventEmitter',
-      version='1.1',
+      version='1.3',
       description='Simple python events library',
       long_description=long_description,
       author='Etienne Tissieres',
       author_email='etienne.tissieres@gmail.com',
       url='https://github.com/etissieres/PyEventEmitter',
       packages=['py_event_emitter'],
-      license='MIT')
+      license='MIT',
+      classifiers=classifiers)
